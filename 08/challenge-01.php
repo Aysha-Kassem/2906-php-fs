14fa9ed5d27be771d8a8249cc4163d5253f1562d
Aysha <AyshaKassem@Aysha-MacBook.local>
2025-02-04 20:54:51 +0200

    add answer of challenge

--- 08/challenge-01.php
+++ 08/challenge-01.php
@@ -1,10 +1,18 @@
 <?php 
 
-$status = 'pending';
+$status = 'Reviewed';
 
-$color = '';
 
-// for next cases, the color should be red => Canclled, Refunded, Complain
+// for next cases, the color should be red => Cancelled, Refunded, Complain
 // for next cases, the color should be blue => Pending, On Hold, Postpond
 // for next cases, the color should be green => Delivered, Shipped, Reviewed
 // handle not matched status
+
+$color = match(strtolower($status)){
+    'cancelled', 'refunded', 'complained' => 'red',
+    'pending', 'on hold', 'postponed' => 'blue',
+    'delivered','shipped','reviewed' => 'green',
+    default => 'not matched status'
+};
+
+echo "<h2 style='color: $color;'>$status</h2>";
