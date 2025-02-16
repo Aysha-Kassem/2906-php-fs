04aa09fdc649b080b695265212e95771f4b9757c
Aysha Kassem <ayshakassem59@gmail.com>
2025-02-16 19:58:36 +0200

    add challenges

--- 11/questions/index.php
+++ 11/questions/index.php
@@ -1,20 +1,20 @@
 <?php
 
-// $directory = __DIR__; // Change this to your folder path
+$directory = __DIR__; // Change this to your folder path
 
-// // Get all PHP files in the directory
-// $files = glob("$directory/*.php");
+// Get all PHP files in the directory
+$files = glob("$directory/*.php");
 
-// var_dump($files);
+var_dump($files);
 
-// foreach ($files as $file) {
-//   $content = file_get_contents($file); // Read file content
+foreach ($files as $file) {
+  $content = file_get_contents($file); // Read file content
 
-//   // Replace any value inside $pattern = "..."
-//   $newContent = preg_replace('/(\$pattern\s*=\s*)"([^"]*)";/', '$1"//";', $content);
+  // Replace any value inside $pattern = "..."
+  $newContent = preg_replace('/(\$pattern\s*=\s*)"([^"]*)";/', '$1"//";', $content);
 
-//   // Save the modified content back to the file
-//   file_put_contents($file, $newContent);
+  // Save the modified content back to the file
+  file_put_contents($file, $newContent);
 
-//   echo "Updated: $file\n";
-// }
+  echo "Updated: $file\n";
+}
